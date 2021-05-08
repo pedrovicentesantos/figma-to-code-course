68a81859c08ea89cc900f30b9bfd1dd7e8d95f92
Pedro Vicente Santos <pedrovicente@poli.ufrj.br>
2021-05-08 20:08:48 -0300

    Fix problem with nav position when changing from mobile to desktop

--- Dashboard/js/index.js
+++ Dashboard/js/index.js
@@ -3,11 +3,9 @@ const backBtn = document.querySelector('.back-btn');
 const menu = document.querySelector('nav');
 
 menuBtn.addEventListener('click', () => {
-  // menu.style.transform = 'translateX(0)';
   menu.classList = 'active';
 });
 
 backBtn.addEventListener('click', () => {
-  // menu.style.transform = 'translateX(-100%)';
   menu.classList = 'hidden';
 });
